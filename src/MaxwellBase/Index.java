c8f6c2510a7a89ad17fca82af9771344155a1906
sailaja.mandadi <lakshmisailajamandadi@gmail.com>
2022-12-03 21:06:36 -0600

    End of 3

---
+++ src/MaxwellBase/Index.java
@@ -0,0 +1,14 @@
+package MaxwellBase;
+
+public class Index {
+
+    public Index(String tableName, String columnName) {
+
+    }
+
+    public Record search(Object value) {
+        return null;
+    }
+
+
+}
